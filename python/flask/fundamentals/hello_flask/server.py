d6897aa1f854aa1c35ce6e9b10483861769cd8a5
Harrison Anthony <harrisoneanthony@gmail.com>
2022-11-01 12:35:11 -0400

    commit

--- python/flask/fundamentals/hello_flask/server.py
+++ python/flask/fundamentals/hello_flask/server.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/") # / means http://127.0.0.1:5001/
 def home():
-    return "Trick or Treat"
+    return render_template("home.html")
 
+@app.route("/say/hello/<name>")
+def say_hello(name):
+    print(name)
+    return render_template("say_hello.html", name = name)
 
 
 if __name__ == "__main__":
