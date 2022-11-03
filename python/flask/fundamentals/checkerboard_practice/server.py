fb3d611ed63eb09425d4563270b543589aeffee3
Harrison Anthony <harrisoneanthony@gmail.com>
2022-11-02 20:23:57 -0400

    commit

--- python/flask/fundamentals/checkerboard_practice/server.py
+++ python/flask/fundamentals/checkerboard_practice/server.py
@@ -10,8 +10,12 @@ def checkerboard_rows(num):
     return render_template("checkerboard_practice.html", num=num)
 
 @app.route("/<int:row>/<int:col>")
-def checkerboard_rows_col(row,col):
+def checkerboard_row_col(row,col):
     return render_template("checkerboard_row_col.html", row=row, col=col)
+
+@app.route("/<int:row>/<int:col>/<string:color1>/<string:color2>")
+def checkerboard_row_col_colors(row,col,color1,color2):
+    return render_template("checkerboard_row_col.html", row=row, col=col, color1=color1, color2=color2)
 
 if __name__ == "__main__":
     app.run(debug=True, port=5001)
