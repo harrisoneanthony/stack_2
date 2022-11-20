c94d2784f32cb974bdab3396577eae672359923e
Harrison Anthony <harrisoneanthony@gmail.com>
2022-11-20 13:27:23 -0500

    commit

--- python/flask_mysql/first_flask_mysql/friend.py
+++ python/flask_mysql/first_flask_mysql/friend.py
@@ -27,6 +27,6 @@ class Friend:
     #  class method to save our friend to the database
     @classmethod
     def save(cls, data):
-        query = "INSERT INTO friends ( first_name, last_name, occupation, createdat, updated_at ) VALUES ( %(fname)s, %(lname)s, %(occ)s, NOW(), NOW() );"
+        query = "INSERT INTO friends ( first_name, last_name, occupation, created_at, updated_at ) VALUES ( %(fname)s, %(lname)s, %(occ)s, NOW(), NOW() );"
         # data is a dictionary that will be passed into the save method from server.py
         return connectToMySQL('first_flask').query_db(query,data)
