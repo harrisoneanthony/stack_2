ef7677a56a774306c0bc7757e05d59183415281d
Harrison Anthony <harrisoneanthony@gmail.com>
2022-12-06 20:22:20 -0500

    commit

---
+++ python/flask_mysql/burgers/flask_app/models/burger.py
@@ -0,0 +1,15 @@
+from flask_app.config.mysqlconnection import connectToMySQL
+
+class Burger:
+    def __init__( self , db_data ):
+        self.id = db_data['id']
+        self.name = db_data['name']
+        self.bun = db_data['bun']
+        self.meat = db_data['meat']
+        self.calories = db_data['calories']
+        self.created_at = db_data['created_at']
+        self.updated_at = db_data['updated']
+    @classmethod
+    def save( cls , data ):
+        query = "INSERT INTO burgers ( name , bun, meat, calories, restaurant_id, created_at , updated_at ) VALUES (%(name)s, %(bun)s, %(meat)s, %(calories)s, %(restaurant_id)s,NOW(),NOW());"
+        return connectToMySQL('burgers').query_db(query,data)
