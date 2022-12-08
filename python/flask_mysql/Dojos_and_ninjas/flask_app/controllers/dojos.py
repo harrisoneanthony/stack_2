eddabec33933853653b72b7c2a17657c33437714
Harrison Anthony <harrisoneanthony@gmail.com>
2022-12-08 17:35:07 -0500

    commit

---
+++ python/flask_mysql/Dojos_and_ninjas/flask_app/controllers/dojos.py
@@ -0,0 +1,13 @@
+from flask import render_template, redirect
+from flask_app import app
+from flask_app.models.dojo import Dojo
+
+
+@app.route('/')
+def index():
+    return redirect('/dojos')
+
+@app.route('/dojos')
+def dojos():
+    dojos = Dojo.get_all()
+    return render_template('index.html', all_dojos = dojos)
