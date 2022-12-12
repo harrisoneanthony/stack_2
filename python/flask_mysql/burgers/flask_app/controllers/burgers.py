e721ad9f66394564647722cb64f017543aaaaecc
Harrison Anthony <harrisoneanthony@gmail.com>
2022-12-12 15:50:13 -0500

    comit

--- python/flask_mysql/burgers/flask_app/controllers/burgers.py
+++ python/flask_mysql/burgers/flask_app/controllers/burgers.py
@@ -1,23 +1,18 @@
 from flask import render_template,redirect,request
 from flask_app import app
-from flask_app.models.burger import Burger
+from flask_app.models import burger, restaurant
 
 @app.route('/')
 def index():
-    return render_template('index.html')
-
-@app.route('/burgers')
-def all_burgers():
-    return render_template('all_burgers.html')
+    all_restaurants = restaurant.Restaurant.get_all()
+    return render_template('index.html', all_restaurants = all_restaurants)
 
 @app.route('/create/burger', methods = ["POST"])
 def create():
-    data = {
-        "name": request.form["name"],
-        "bun": request.form["bun"],
-        "meat": request.form["meat"],
-        "calories": request.form["calories"],
-        "restaurant": request.form["restaurant_id"],
+    burger.Burger.save(request.form)
+    return redirect('/burgers')
 
-    }
-    return redirect('/burgers', data = data)
+
+@app.route('/burgers')
+def show_all_burgers():
+    return render_template('all_burgers.html', all_burgers=burger.Burger.get_all_burgers())
