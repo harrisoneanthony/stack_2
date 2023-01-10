73b365b800fa36424c310b62494c8fe034d748b9
Harrison Anthony <harrisoneanthony@gmail.com>
2023-01-09 19:17:12 -0500

    commit

--- python/flask_mysql/friendships/flask_app/controllers/friendships_controllers.py
+++ python/flask_mysql/friendships/flask_app/controllers/friendships_controllers.py
@@ -6,5 +6,4 @@ from flask_app.models.friendships_models import Friendship
 @app.route('/friendship/create', methods=['POST'])
 def create_friendship():
     Friendship.create_friendship(request.form)
-    print(Friendship.create_friendship(request.form))
     return redirect('/')
